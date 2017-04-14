a913a767bf4e4eb6be2b41564a068a4195ed998b
SyrtisGecko <syrtismajor@gmail.com>
2017-04-14 19:45:14 +0200

    Created new activity with FrameLayout

--- app/src/main/java/com/example/przemek/hsport/myapplication2/Main6Activity_withFrameLayout.java
+++ app/src/main/java/com/example/przemek/hsport/myapplication2/Main6Activity_withFrameLayout.java
@@ -1,15 +1,13 @@
 package com.example.przemek.hsport.myapplication2;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.widget.TableLayout;
 
-public class Main5Activity_withTableLayout extends Activity {
+public class Main6Activity_withFrameLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main5_with_table_layout);
+        setContentView(R.layout.activity_main6_with_frame_layout);
     }
-
 }
