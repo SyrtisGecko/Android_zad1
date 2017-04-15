acb2351bc5d6b3bbbd56da5161ef44f4febb70d4
SyrtisGecko <syrtismajor@gmail.com>
2017-04-15 16:20:35 +0200

    Implemented some animations of an image in Main3Activity

--- app/src/main/java/com/example/przemek/hsport/myapplication2/Main3Activity.java
+++ app/src/main/java/com/example/przemek/hsport/myapplication2/Main3Activity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Main3Activity extends Activity {
@@ -21,5 +24,41 @@ public class Main3Activity extends Activity {
     protected void onStop() {
         super.onStop();
         Toast.makeText(getApplicationContext(), "Activity_3 has been stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clockwise(View view) {
+        ImageView image = (ImageView) findViewById(R.id.imageView3);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.clockwise);
+        image.startAnimation(animation);
+    }
+
+    public void zoom(View view) {
+        ImageView image = (ImageView) findViewById(R.id.imageView3);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanimation);
+        image.startAnimation(animation);
+    }
+
+    public void fade(View view) {
+        ImageView image = (ImageView) findViewById(R.id.imageView3);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        image.startAnimation(animation);
+    }
+
+    public void blink(View view) {
+        ImageView image = (ImageView) findViewById(R.id.imageView3);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+        image.startAnimation(animation);
+    }
+
+    public void move(View view) {
+        ImageView image = (ImageView) findViewById(R.id.imageView3);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+        image.startAnimation(animation);
+    }
+
+    public void slide(View view) {
+        ImageView image = (ImageView) findViewById(R.id.imageView3);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide);
+        image.startAnimation(animation);
     }
 }
