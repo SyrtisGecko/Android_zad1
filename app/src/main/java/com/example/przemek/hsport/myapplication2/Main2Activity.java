ddfeb54b6a635acd3f582036275b41bd83caea6e
SyrtisGecko <syrtismajor@gmail.com>
2017-04-15 09:54:46 +0200

    Implemented sending email Intent to button in Main2Activity

--- app/src/main/java/com/example/przemek/hsport/myapplication2/Main2Activity.java
+++ app/src/main/java/com/example/przemek/hsport/myapplication2/Main2Activity.java
@@ -1,6 +1,8 @@
 package com.example.przemek.hsport.myapplication2;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,5 +22,14 @@ public class Main2Activity extends Activity {
                 return false;
             }
         });
+    }
+
+    public void sendEmail(View view) {
+        Intent email = new Intent(Intent.ACTION_SEND, Uri.parse("mailto:"));
+        email.putExtra(Intent.EXTRA_EMAIL, new String[] {"syrtismajor@gmail.com"});
+        email.putExtra(Intent.EXTRA_SUBJECT, "Android App Test");
+        email.putExtra(Intent.EXTRA_TEXT, "Sending email using Intent attached to button in Activity2");
+        email.setType("message/rfc822");
+        startActivity(Intent.createChooser(email, "Choose email client..."));
     }
 }
